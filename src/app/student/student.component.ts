9fa873bc36186ebf26f540bb76a902f5d34c85ef
davidssm97 <dssalazar@tcc.com.co>
2024-06-26 17:07:24 -0500

    se crea el proyecto

---
+++ src/app/student/student.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './student.component.html',
+  styleUrl: './student.component.css'
+})
+export class StudentComponent {
+
+}
